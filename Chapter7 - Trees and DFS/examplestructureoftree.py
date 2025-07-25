7d23144ce354def56501087aa3253eabfaea89da
Abhyudai Bisen <abhyudai.bisen.22@gmail.com>
2025-07-25 11:56:07 +0530

    Update examplestructureoftree.py

--- Chapter7 - Trees and DFS/examplestructureoftree.py
+++ Chapter7 - Trees and DFS/examplestructureoftree.py
@@ -1,11 +1,11 @@
-class folder:
-    def __init__(self,name:str,contents=[]):
-        self.name=name
+class folder:                                       # using classes for this because, why not? 
+    def __init__(self,name:str,contents=[]):        # i don't know OOP that well so i try to 
+        self.name=name                              # use it whereever to kind of, exercise it 
         self.contents=contents
         for i in contents:
             if type(i) != folder and type (i) != file:
                 raise TypeError("contents of a folder can only ever be other folders and files")
-    def addContent(self,item):                                                                                              #TO DO 24-07-25 : Add a path of file functionality to rDF() in method 1
+    def addContent(self,item):                                                                                              #TO DO 24-07-25 : Add a path of file functionality to rDF()
         if type(item) != folder and type(item) != file:
             raise TypeError("Contents of a folder can only be other folders and files")    
         self.contents.append(item)
