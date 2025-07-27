ce577d4a1f222a1f420909269363e4e3e17d4cf7
Abhyudai Bisen <abhyudai.bisen.22@gmail.com>
2025-07-27 12:28:30 +0530

    Update one_line_flex.py

--- one_line_flex.py
+++ one_line_flex.py
@@ -1,1 +1,2 @@
-print("\n".join(["*"*j for j in range(1,int(input("How many rows?\n "))+1)]))
+print("\n".join(["*"*j for j in range(1,int(input("How many rows?\n "))+1)])) # wowowowow you know comprehensiom?????!!??!?!?!1 no effing way dyude !!!!   
+                                                                              # dumbass
